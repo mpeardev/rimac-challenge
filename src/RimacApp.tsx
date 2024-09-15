ae05dc1c9e5deeeb6df70e7b403ffb5c6f29bb5f
Mirko Peramás <mirkoperamas@MacBook-Pro-de-Mirko.local>
2024-09-15 09:03:47 -0500

    remove 404 page && add hassRouter

--- src/RimacApp.tsx
+++ src/RimacApp.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { AppRoutes } from "./modules/core/router/routes";
 
 export default function RimacApp() {
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import rewriteAll from "vite-plugin-rewrite-all";
-import path from "path-browserify";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), rewriteAll()],
+  plugins: [react()],
   base: "/rimac-challenge/",
   build: {
     outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: path.resolve("index.html"),
-        404: path.resolve("404.html"),
-      },
-    },
   },
 });
